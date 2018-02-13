fb6b9c1b3c5be83b5c4f4238472336b24e86ed58
Yaming Cheng <ycheng@lcs.on.ca>
2018-02-12 22:27:34 -0500

    Finish first exercise

--- Collection Types.playground/Pages/Exercise One.xcplaygroundpage/Contents.swift
+++ Collection Types.playground/Pages/Exercise One.xcplaygroundpage/Contents.swift
@@ -8,23 +8,31 @@
  */
 
 import Foundation
-
+var grades: [Double] = []
 
 /*:
  2. Now populate the array with eight random grades between 60 and 100.
  */
 
-
+for _ in 1...8 {
+    grades.append( Double (arc4random_uniform(40) + 60))
+}
 /*:
  3. Print a list of each array index and the associated value.
  */
-
-
+for (index, value) in grades.enumerated() {
+    print("At index \(index) the value is \(value)")
+}
 /*:
  4. Determine and print the highest grade.
  */
-
-
+var highest = 61.0
+for grade in grades {
+    if grade > highest {
+        highest = grade
+    }
+}
+print("The highest grade is \(highest).")
 //: [Next](@next)
 
 
